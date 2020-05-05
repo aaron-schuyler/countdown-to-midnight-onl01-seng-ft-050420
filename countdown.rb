89e3c342426b8705bbbd2a262dd1faade2be915c
Aaron Schuyler <asky28@gmail.com>
2020-05-05 14:18:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,14 @@
 def countdown(i)
   while i > 0 do
     puts "#{i} SECOND(s)!"
+    i-=1
+  end
+ puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(i)
+  while i > 0 do
+    puts "#{i} SECOND(s)!"
     sleep(1)
     i-=1
   end
